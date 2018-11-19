7fee647905dfda492cc57d61f4cff58da703c227
KuangPF <1633397286@qq.com>
2018-11-19 21:47:33 +0800

    feat: add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# sudo chmod a+x deploy.sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/MPComponent/mpvue-weui.git master:gh-pages
+
+cd -
