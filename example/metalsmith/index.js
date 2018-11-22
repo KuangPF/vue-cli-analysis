dc3ab3576114ff901fce30da3874d53de7ac252e
KuangPF <1633397286@qq.com>
2018-11-23 00:29:00 +0800

    feat: metalsmith demo done

--- example/metalsmith/index.js
+++ example/metalsmith/index.js
@@ -3,11 +3,28 @@ const Inquirer = require('inquirer')
 const Render = require('consolidate').handlebars.render
 const async = require('async')
 
+// Support types from prompt-for which was used before
+const promptMapping = {
+  string: 'input',
+  boolean: 'confirm'
+}
+
+// 定义 prompts
+var prompts = {
+  name: {
+    type: 'string',
+    required: true,
+    message: 'Project name',
+  },
+  description: {
+    type: 'string',
+    required: false,
+    message: 'Project description',
+    default: 'A Vue.js project',
+  },
+}
 Metalsmith(__dirname)
-  .metadata({
-    name: 'hello metalsmith',
-    description: 'some description'
-  })
+  .use(askQuestion())
   .use(renderTemplateFiles())
   .source('./templates')
   .destination('./src')
@@ -17,8 +34,39 @@ Metalsmith(__dirname)
 
 function askQuestion() {
   return (file, metalsmith, done) => {
-    
+    async.eachSeries(Object.keys(prompts), (key, next) => {
+      prompt(metalsmith.metadata(), key, prompts[key], next)
+    }, done)
   }
+}
+
+function prompt(data, key, prompt, done) {
+  let promptDefault = prompt.default
+  if (typeof prompt.default === 'function') {
+    promptDefault = function () {
+      return prompt.default.bind(this)(data)
+    }
+  }
+  Inquirer.prompt([{
+    type: promptMapping[prompt.type] || prompt.type,
+    name: key,
+    message: prompt.message || prompt.label || key,
+    default: promptDefault,
+    choices: prompt.choices || [],
+    validate: prompt.validate || (() => true)
+  }]).then(answers => {
+    if (Array.isArray(answers[key])) {
+      data[key] = {}
+      answers[key].forEach(multiChoiceAnswer => {
+        data[key][multiChoiceAnswer] = true
+      })
+    } else if (typeof answers[key] === 'string') {
+      data[key] = answers[key].replace(/"/g, '\\"')
+    } else {
+      data[key] = answers[key]
+    }
+    done()
+  }).catch(done)
 }
 
 function renderTemplateFiles() {
@@ -26,7 +74,7 @@ function renderTemplateFiles() {
     const keys = Object.keys(files)
     const metalsmithMetadata = metalsmith.metadata()
     async.each(keys, (file, next) => {
-     
+
       const str = files[file].contents.toString()
       // do not attempt to render files that do not have mustaches
       if (!/{{([^{}]+)}}/g.test(str)) {
