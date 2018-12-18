11b38f2c9fbba2ffdab6d9c80e497848b8bb9160
KuangPF <1633397286@qq.com>
2018-12-18 22:49:30 +0800

    test: class API

--- test/create.js
+++ test/create.js
@@ -2,4 +2,21 @@ function camelize (str) {
   return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
 }
 
-console.log(camelize('user-name'))
+// console.log(camelize('user-name'))
+
+const cli = cli=> {
+  cli.say('vue cli')
+}
+
+class API {
+  constructor() {
+
+  }
+  say(msg) {
+    console.log(msg)
+  }
+}
+
+const api = new API()
+console.log(cli(api))
+// cli(api)
