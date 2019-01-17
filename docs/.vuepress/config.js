3aa2d500c8637f81def8126270a068c6b20a7a27
KuangPF <1633397286@qq.com>
2019-01-17 16:54:05 +0800

    feat: update vue-cli-service navBar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,8 +28,8 @@ module.exports = {
             text: '内置插件', items: [
               {text: 'serve', link: '/cli-service/serve'},
               {text: 'build', link: '/cli-service/build'},
-              /*{text: 'inspect', link: '/cli-service/inspect'},
-              {text: 'help', link: '/cli-service/help'},
+              {text: 'inspect', link: '/cli-service/inspect'}
+              /*{text: 'help', link: '/cli-service/help'},
               {text: 'base', link: '/cli-service/base'},
               {text: 'css', link: '/cli-service/css'},
               {text: 'dev', link: '/cli-service/dev'},
