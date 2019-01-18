81dae328ff97e8e881aa32eab33e3aa5e7bbf95b
KuangPF <1633397286@qq.com>
2019-01-18 14:45:05 +0800

    feat: command config

--- packages/@vue/cli/lib/config.js
+++ packages/@vue/cli/lib/config.js
@@ -4,11 +4,11 @@ const homedir = require('os').homedir()
 const { get, set, unset, error, launch } = require('@vue/cli-shared-utils')
 
 async function config (value, options) {
-  const file = path.resolve(homedir, '.vuerc')
-  const config = await fs.readJson(file)
+  const file = path.resolve(homedir, '.vuerc') // 获取 .vuerc 路径
+  const config = await fs.readJson(file) // 获取 .vuerc 中的内容
 
-  if (!options.delete && !options.get && !options.edit && !options.set) {
-    if (options.json) {
+  if (!options.delete && !options.get && !options.edit && !options.set) { // 输出 .vuerc 内容
+    if (options.json) { // --json
       console.log(JSON.stringify({
         resolvedPath: file,
         content: config
@@ -18,7 +18,21 @@ async function config (value, options) {
     }
   }
 
-  if (options.get) {
+
+  /*exports.get = function (target, path) {
+    const fields = path.split('.')
+    let obj = target
+    const l = fields.length
+    for (let i = 0; i < l - 1; i++) {
+      const key = fields[i]
+      if (!obj[key]) {
+        return undefined
+      }
+      obj = obj[key]
+    }
+    return obj[fields[l - 1]]
+  }*/
+  if (options.get) { // 获取 .vuerc 中某个指定的配置，vue config -g presets 可以获取预设的值
     const value = get(config, options.get)
     if (options.json) {
       console.log(JSON.stringify({
@@ -29,7 +43,7 @@ async function config (value, options) {
     }
   }
 
-  if (options.delete) {
+  if (options.delete) { // 删除 .vuerc 中某个指定的配置
     unset(config, options.delete)
     await fs.writeFile(file, JSON.stringify(config, null, 2), 'utf-8')
     if (options.json) {
@@ -41,7 +55,7 @@ async function config (value, options) {
     }
   }
 
-  if (options.edit) {
+  if (options.edit) { // 启用编辑器编辑 .vuerc
     launch(file)
   }
 
@@ -49,7 +63,7 @@ async function config (value, options) {
     throw new Error(`Make sure you define a value for the option ${options.set}`)
   }
 
-  if (options.set && value) {
+  if (options.set && value) { // 设置选项
     set(config, options.set, value)
 
     if (value.match('[0-9]')) {
