f6fb9beed8a197331a7d1f41adbea7e9b588661e
KuangPF <1633397286@qq.com>
2019-01-31 23:16:39 +0800

    feat: express.static middleware

--- packages/@vue/cli-ui/apollo-server/server.js
+++ packages/@vue/cli-ui/apollo-server/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+// 引入 express-history-api-fallback 中间件，避免在 History 路由模式下出现 404 的情况
 const fallback = require('express-history-api-fallback')
 // Connectors
 const clientAddons = require('./connectors/client-addons')
@@ -9,8 +10,8 @@ const distPath = path.resolve(__dirname, '../dist')
 const publicPath = path.resolve(__dirname, '../ui-public')
 
 module.exports = app => {
-  app.use(express.static(distPath, { maxAge: 0 }))
-  app.use('/public', express.static(publicPath, { maxAge: 0 }))
+  app.use(express.static(distPath, { maxAge: 0 })) // express.static 内置中间件函数，设置静态资源（文件）目录
+  app.use('/public', express.static(publicPath, { maxAge: 0 }))  // public 资源目录，e.g. http://localhost:8000/public/vue-cli.png
   app.use('/_plugin/:id/*', plugins.serve)
   app.use('/_plugin-logo/:id', plugins.serveLogo)
   app.use('/_addon/:id/*', clientAddons.serve)
